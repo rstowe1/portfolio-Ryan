6b8bdfe95fb700f6eb977a3b4a592de1c3f9f7b1
rstowe1 <ryanwstowe@gmail.com>
2019-09-21 22:00:38 -0400

    'wip'

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+
 import {getCookieFromReq} from "../helpers/utils";
 
 const setAuthHeader = (req) => {
@@ -11,7 +12,11 @@ const setAuthHeader = (req) => {
 };
 
 export const getSecretData = async (req) => {
-
-  const url ='http://localhost:3000/api/v1/secret';
+  const url = 'http://localhost:3000/api/v1/secret';
   return await axios.get(url, setAuthHeader(req)).then(response => response.data);
 };
+
+export const getPortfolios = async (req) => {
+  return await axios.get('http://localhost:3000/api/v1/portfolios', setAuthHeader(req)).then(response => response);
+};
+
--- pages/portfolios.js
+++ pages/portfolios.js
@@ -3,35 +3,32 @@ import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import {Link} from '../routes'
 import {Col, Row, Card, CardHeader, CardBody, CardText, CardTitle} from 'reactstrap';
-import axios from 'axios';
+import {getPortfolios} from '../actions';
 
 class Portfolios extends React.Component {
 
-  static async getInitialProps() {
-    let post = [];
-
+  static async getInitialProps({req}) {
+    let portfolios = [];
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      post = response.data;
-    } catch (e) {
-      console.error(e)
+      portfolios = await getPortfolios(req);
+    } catch (err) {
+      console.error(err)
     }
-
-    return {posts: post.splice(0, 10)};
+    return {portfolios};
   }
 
-  renderPosts(posts) {
-    return posts.map((post, index) => {
+  renderPortfolios(portfolios) {
+    return portfolios.map((portfolio, index) => {
       return (
         <Col md="4">
           <React.Fragment key={index}>
             <span>
               <Card className="portfolio-card">
-                <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                <CardHeader className="portfolio-card-header"> {portfolio.position} </CardHeader>
                 <CardBody>
-                  <p className="portfolio-card-city"> Some Location {index} </p>
-                  <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
-                  <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                  <p className="portfolio-card-city"> {portfolio.location} </p>
+                  <CardTitle className="portfolio-card-title"> {portfolio.title} </CardTitle>
+                  <CardText className="portfolio-card-text"> {portfolio.description} </CardText>
                   <div className="readMore"> </div>
                 </CardBody>
               </Card>
@@ -45,12 +42,12 @@ class Portfolios extends React.Component {
 
 
   render() {
-    const {posts} = this.props;
+    const {portfolios} = this.props;
     return (
       <BaseLayout {...this.props.auth}>
         <BasePage className='portfolio-page' title='Portfolios'>
           <Row>
-            {this.renderPosts(posts)}
+            {this.renderPortfolios(portfolios)}
           </Row>
         </BasePage>
       </BaseLayout>
