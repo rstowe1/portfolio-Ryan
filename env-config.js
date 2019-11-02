09d2482cbd946a05837ad991fd57d826f11a6ad9
rstowe1 <ryanwstowe@gmail.com>
2019-11-02 13:33:58 -0400

    'wip'

--- env-config.js
+++ env-config.js
@@ -4,4 +4,4 @@ module.exports = {
   'process.env.BASE_URL': prod ? 'https://portfolio-stowe.herokuapp.com' : 'http://localhost:3000',
   'process.env.NAMESPACE': 'https://portfolio-stowe.herokuapp.com',
   'process.env.CLIENT_ID': 'It9itOzefx06xsg2RdrcIIEPCKf64Sot'
-}
+};
