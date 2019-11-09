5587e6517dd10bf6f9c81e5321aa0c91a9e2c9a5
rstowe1 <ryanwstowe@gmail.com>
2019-11-09 13:46:54 -0500

    fixing CSS Optimization error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-// next.config.js
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
-const OptimizeCSSAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = withCSS(withSass({
-  webpack(config,{dev}) {
-    if(config.mode ==='production') {
-      if(Array.isArray(config.optimization.minimizer)) {
+  webpack(config, {dev}) {
+    if (config.mode === 'production') {
+      if (Array.isArray(config.optimization.minimizer)) {
         config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
       }
     }
