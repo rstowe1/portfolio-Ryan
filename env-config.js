42085620fbdabffcc2073b5e8866062440ee5316
rstowe1 <ryanwstowe@gmail.com>
2019-11-09 16:22:21 -0500

    finishing up

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BASE_URL': prod ? 'https://portfolio-stowe.herokuapp.com' || 'https://ryanw.tech' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://portfolio-stowe.herokuapp.com' || 'https://ryanw.tech',
+  'process.env.NAMESPACE': 'https://ryanw.tech',
   'process.env.CLIENT_ID': 'It9itOzefx06xsg2RdrcIIEPCKf64Sot'
 };
