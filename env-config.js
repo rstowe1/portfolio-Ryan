d77cf71776a1e01d35b611e428f2576503008c0c
rstowe1 <32300186+rstowe1@users.noreply.github.com>
2019-11-11 14:58:28 -0500

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://www.ryanw.tech' : 'http://localhost:3000',
-  'process.env.NAMESPACE':  'https://www.ryanw.tech',
+  'process.env.BASE_URL': prod ? 'https://www.ryanw.tech/' : 'http://localhost:3000',
+  'process.env.NAMESPACE':  'https://portfolio-stowe.herokuapp.com',
   'process.env.CLIENT_ID': 'It9itOzefx06xsg2RdrcIIEPCKf64Sot'
 };
