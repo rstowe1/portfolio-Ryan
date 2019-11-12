bc6eda02b16a4da95c3232d0f1f594d75f2ec433
rstowe1 <ryanwstowe@gmail.com>
2019-11-12 18:26:07 -0500

    fixing merge conflicts

--- env-config.js
+++ env-config.js
@@ -1,11 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-<<<<<<< HEAD
-  'process.env.BASE_URL': prod ? 'https://ryanw.tech' : 'http://localhost:3000',
-=======
   'process.env.BASE_URL': prod ? 'https://www.ryanw.tech' : 'http://localhost:3000',
->>>>>>> 9e0e3f80503f8fbe4ce7c4c1f3d1a827bdeb58fe
   'process.env.NAMESPACE':  'https://portfolio-stowe.herokuapp.com',
   'process.env.CLIENT_ID': 'It9itOzefx06xsg2RdrcIIEPCKf64Sot'
 };
